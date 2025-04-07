9aaa61e7d835bdb22f8c144ac70b62f11afe8b85
Ronaldo Cysne <ronaldocysne@gmail.com>
2025-04-07 16:38:22 -0300

    Add files via upload

--- src/app/(public)/login/login.tsx
+++ src/app/(public)/login/login.tsx
@@ -7,12 +7,12 @@ import { InputField } from "@/app/components/Input/input";
 export default function Login() {
     return (
         <div className="bg-gray-900 h-screen max-w-3xl mx-auto w-full">
-            <form action="" className="w-full h-full flex flex-col justify-center items-center">
-                <h1 className=" text-gray-400">PÁGINA DE LOGIN</h1>
-                <InputField>Email</InputField>
-                <InputField>Senha</InputField>
-                <Button className="max-w-[14rem]">Clique Aqui</Button>
-                <Button className="max-w-[10rem]">Esqueceu a senha.</Button>
+            <form action="" className="w-full h-full flex flex-col justify-center items-center gap-6">
+                <h1 className=" text-gray-400">LOGIN</h1>
+                <InputField>email</InputField>
+                <InputField>senha</InputField>
+                <Button className="max-w-[10rem] mt-2.5 border outline outline-amber-50">Clique Aqui</Button>
+                <Button className="max-w-[10rem] border outline outline-amber-50">Esqueceu a senha.</Button>
             </form>
 
         </div>
--- src/app/components/Input/input.tsx
+++ src/app/components/Input/input.tsx
@@ -1,6 +1,8 @@
 import { Input } from "@/components/ui/input"
 
-export function InputField({children}) {
-    return <Input className="w-[30rem]" type={children} placeholder={children} />
+
+export function InputField({ children }) {
+
+    return <Input className="w-[15rem] text-zinc-200 border-0 border-b-2 rounded-none border-solid border-amber-50" type={children} placeholder={children} />
 }
 
